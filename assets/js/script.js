8c1dfbc2902a50cd1ea8333b4aed2e3b4186ab55
Annah <mwanzamusenya8@gmail.com>
2024-10-21 01:03:19 +0300

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+
+const colorbutton:HTMLElement= document.getElementById("color button")
+colorbutton.addEventListener(type; click ,Listener() void=>
+{
+
+    colorbutton.style.backgroundColor = colorbutton.style.backgroundColor='blue',
+
+}];
